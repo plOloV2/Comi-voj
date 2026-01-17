763d628fa4f318f5b885f82e20ba6bb277189537
pl_OloV2 <ak.dziagwa@gmail.com>
2026-01-17 23:45:35 +0100

    first commit

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main(int argc, char** argv){
+
+    if(argc != 3 && argc != 1)
+        return 1;
+
+    int points_num, trials;
+
+    if(argc == 1)
+    
+
+    return 0;
+
+}
